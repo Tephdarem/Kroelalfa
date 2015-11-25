bbb478fb16c32bfd424b739f0c7e52493b7b5b02
Mikkel Vestergaard <baskernet@gmail.com>
2015-11-25 22:52:00 +0100

    Updated for new branch test

--- Kroelalfa/src/dk/kroelalfa/chat/Main.java
+++ Kroelalfa/src/dk/kroelalfa/chat/Main.java
@@ -15,6 +15,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Så er der hul igennem til GIT!");
+		System.out.println("Commit til Mikkels branch!");
 	}
 
 }
