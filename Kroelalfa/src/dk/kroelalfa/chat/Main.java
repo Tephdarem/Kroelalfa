180b060c604bcad7190cabeef29281245e59a2e6
Mikkel Vestergaard <baskernet@gmail.com>
2015-11-25 22:21:03 +0100

    Initial commit

---
+++ Kroelalfa/src/dk/kroelalfa/chat/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package dk.kroelalfa.chat;
+
+/**
+ * @author Mikkel Vestergaard
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
