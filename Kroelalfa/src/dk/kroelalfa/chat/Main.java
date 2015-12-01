d4132667e77cad49f3ff4270c047f41a517b1282
Mikkel Vestergaard <baskernet@gmail.com>
2015-12-01 12:59:31 +0100

    Changed this project to be java client for REST web service. It can now call a hello service in server project.

--- Kroelalfa/src/dk/kroelalfa/chat/Main.java
+++ Kroelalfa/src/dk/kroelalfa/chat/Main.java
@@ -2,6 +2,14 @@
  * 
  */
 package dk.kroelalfa.chat;
+import java.net.URI;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.jersey.client.ClientConfig;
 
 /**
  * @author Mikkel Vestergaard
@@ -16,6 +24,37 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Så er der hul igennem til GIT!");
 		System.out.println("Commit til Mikkels branch!");
-	}
+		
+		ClientConfig config = new ClientConfig();
+
+	    Client client = ClientBuilder.newClient(config);
+
+	    WebTarget target = client.target(getBaseURI());
+
+	    String response = target.path("rest").
+	              path("hello").
+	              request().
+	              accept(MediaType.TEXT_PLAIN).
+	              get(Response.class)
+	              .toString();
+
+
+	    String plainAnswer = 
+	        target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(String.class);
+	    String xmlAnswer = 
+	        target.path("rest").path("hello").request().accept(MediaType.TEXT_XML).get(String.class);
+	    String htmlAnswer= 
+	        target.path("rest").path("hello").request().accept(MediaType.TEXT_HTML).get(String.class);
+
+	    System.out.println(response);
+	    System.out.println(plainAnswer);
+	    System.out.println(xmlAnswer);
+	    System.out.println(htmlAnswer);
+	  }
+
+	  private static URI getBaseURI() {
+		  //http://localhost:8080/Kroelalfa-server/rest/hello
+	    return UriBuilder.fromUri("http://localhost:8080/Kroelalfa-server").build();
+	  }
 
 }
