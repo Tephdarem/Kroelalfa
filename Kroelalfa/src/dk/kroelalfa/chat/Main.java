73f44b9af19c0dcb4a85e229be4998a4ae0cf0a6
Mikkel Vestergaard <baskernet@gmail.com>
2015-12-01 15:49:32 +0100

    Added support for JSON

--- Kroelalfa/src/dk/kroelalfa/chat/Main.java
+++ Kroelalfa/src/dk/kroelalfa/chat/Main.java
@@ -2,6 +2,7 @@
  * 
  */
 package dk.kroelalfa.chat;
+
 import java.net.URI;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -10,6 +11,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import org.glassfish.jersey.client.ClientConfig;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * @author Mikkel Vestergaard
@@ -22,39 +25,40 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Så er der hul igennem til GIT!");
-		System.out.println("Commit til Mikkels branch!");
-		
+
 		ClientConfig config = new ClientConfig();
 
-	    Client client = ClientBuilder.newClient(config);
+		Client client = ClientBuilder.newClient(config);
 
-	    WebTarget target = client.target(getBaseURI());
+		WebTarget target = client.target(getBaseURI());
 
-	    String response = target.path("rest").
-	              path("hello").
-	              request().
-	              accept(MediaType.TEXT_PLAIN).
-	              get(Response.class)
-	              .toString();
+		String response = target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(Response.class)
+				.toString();
 
+		String plainAnswer = target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(String.class);
+		String xmlAnswer = target.path("rest").path("hello").request().accept(MediaType.TEXT_XML).get(String.class);
+		String htmlAnswer = target.path("rest").path("hello").request().accept(MediaType.TEXT_HTML).get(String.class);
 
-	    String plainAnswer = 
-	        target.path("rest").path("hello").request().accept(MediaType.TEXT_PLAIN).get(String.class);
-	    String xmlAnswer = 
-	        target.path("rest").path("hello").request().accept(MediaType.TEXT_XML).get(String.class);
-	    String htmlAnswer= 
-	        target.path("rest").path("hello").request().accept(MediaType.TEXT_HTML).get(String.class);
+		String jsonAnswer = target.path("rest").path("hello").request().accept(MediaType.APPLICATION_JSON).get(String.class);
 
-	    System.out.println(response);
-	    System.out.println(plainAnswer);
-	    System.out.println(xmlAnswer);
-	    System.out.println(htmlAnswer);
-	  }
+		System.out.println(response);
+		System.out.println(plainAnswer);
+		System.out.println(xmlAnswer);
+		System.out.println(htmlAnswer);
+		JSONObject testJson = null;
+		try {
+			testJson = new JSONObject(jsonAnswer);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (testJson != null)
+			System.out.println(testJson.toString());
+	}
 
-	  private static URI getBaseURI() {
-		  //http://localhost:8080/Kroelalfa-server/rest/hello
-	    return UriBuilder.fromUri("http://localhost:8080/Kroelalfa-server").build();
-	  }
+	private static URI getBaseURI() {
+		// http://localhost:8080/Kroelalfa-server/rest/hello
+		return UriBuilder.fromUri("http://localhost:8080/Kroelalfa-server").build();
+	}
 
 }
